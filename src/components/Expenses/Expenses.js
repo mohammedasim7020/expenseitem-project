897b4776af41f304f0e85231f087dda2ddbeab98
Mohammed-Asim-Feroz <asimferoz555@gmail.com>
2022-10-26 22:34:52 +0530

    add espense item project

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,32 @@
+import React from "react";
+import './Expenses.css'
+import ExpenseItems from "./ExpenseItems";
+import Card from "../UI/Card";
+const Expenses = (props) => {
+  return (
+    <Card className="expenses">
+      <ExpenseItems
+        title={props.expenses[0].title}
+        amount={props.expenses[0].amount}
+        date={props.expenses[0].date}
+      />
+      <ExpenseItems
+        title={props.expenses[1].title}
+        amount={props.expenses[1].amount}
+        date={props.expenses[1].date}
+      />
+      <ExpenseItems
+        title={props.expenses[2].title}
+        amount={props.expenses[2].amount}
+        date={props.expenses[2].date}
+      />
+      <ExpenseItems
+        title={props.expenses[3].title}
+        amount={props.expenses[3].amount}
+        date={props.expenses[3].date}
+      />
+    </Card>
+  );
+};
+
+export default Expenses;
